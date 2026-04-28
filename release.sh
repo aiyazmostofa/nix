531ae00fa8c401310629967497fe75ab7d9f8edc
Aiyaz Mostofa <127061437+aiyazmostofa@users.noreply.github.com>
2026-04-28 06:56:55 -0500

    Starting fresh

--- release.sh
+++ release.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 set -ev
-HASH=$(sha256sum result/bin/nix | awk '{ print $1 }')
+NEW_HASH=$(sha256sum result/bin/nix | awk '{ print $1 }')
+OLD_HASH=$(grep 'sha256 "' Formula/nix-static.rb | cut -d '"' -f 2 || echo "")
+if [ "$NEW_HASH" = "$OLD_HASH" ]; then
+  echo "Binary hasn't changed. Skipping update."
+  exit 0
+fi
 cat <<EOF > Formula/nix-static.rb
 class NixStatic < Formula
-  desc "Static build of the Nix package manager"
+  desc "Static build of the Nix CLI"
   homepage "https://nixos.org"
   url "https://github.com/${GITHUB_REPOSITORY}/releases/download/${GITHUB_RUN_ID}/nix"
   version "${GITHUB_RUN_ID}"
-  sha256 "${HASH}"
+  sha256 "${NEW_HASH}"
   def install; bin.install "nix"; end
 end
 EOF
@@ -16,3 +21,4 @@ git config user.email "github-actions[bot]@users.noreply.github.com"
 git add Formula/nix-static.rb
 git commit -m "Update to ${GITHUB_RUN_ID}" || echo "No changes"
 git push
+gh release create "${GITHUB_RUN_ID}" result/bin/nix
